56e202478af14f8f788b02ee55cc26f1b9745e0a
h4ck3r <h4ck3r@dhost>
2024-09-05 22:19:51 -0400

    updated python

---
+++ languages/python/exfiles/ex6-operator.py
@@ -0,0 +1,29 @@
+x = 10
+y = 5
+
+print("If x = 10 and y = 5:")
+
+z = x+y
+print("1:x+y is", z)
+
+z += x
+print("2: += x is", z)
+
+z *= x
+print("3: *= x is", z)
+
+z /= x
+print("4: /= x is", z)
+
+a = 2
+
+print("Given a, x = 2, 10:")
+
+a %= x
+print("6: a %= x is", a)
+
+a **= x
+print("6: **= x is", a)
+
+a //= x
+print("7: //= x is", a)
