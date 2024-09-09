27437c557dfcd476afcaedbc01c05b48ad0a8cbf
h4ck3r <h4ck3r@dhost>
2024-09-09 11:23:14 -0400

    updated script python

---
+++ languages/python/exfiles/ex7-operators.py
@@ -0,0 +1,13 @@
+number1 = 100 > 50
+number2 = 100 < 50
+number3 = 100 == 50
+number4 = 100 != 50
+number5 = 100 >= 50
+number6 = 100 <= 50
+
+print("1. Value of number1:", number1)
+print("2. Value of number2:", number2)
+print("3. Value of number3:", number3)
+print("4. Value of number4:", number4)
+print("5. Value of number5:", number5)
+print("6. Value of number6:", number6)
---
+++ languages/python/exfiles/ex9-membership.py
@@ -0,0 +1,11 @@
+x = "Python Programming"
+
+y = {1: "h", 2: "m"}
+
+print("y" in x)
+
+print("o" not in x)
+
+print(1 in y)
+
+print("a" in y)
