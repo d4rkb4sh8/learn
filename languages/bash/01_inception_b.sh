1dc4402ad4556bdb1cc37a0952efed2ff2038203
h4ck3r <h4ck3r@dhost>
2024-09-12 21:43:09 -0400

    updated

--- languages/bash/01_inception_b.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Your name?" >&2;
-read name;
-  echo "Hello, $name";
--- languages/bash/02_commands_and_arguments.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-read -p "Your name?" name
-if [[ $name = $USER ]]; then
-  echo "Lets master the craft"
-else
-  echo "you are not $USER. Goodbye!"
-fi
